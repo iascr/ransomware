f7d47a244b1e22a984018c43eac1c6768883d8bd
iascr <45016771+iascr@users.noreply.github.com>
2023-11-05 17:03:00 -0600

    Update desencriptar.py

--- desencriptar.py
+++ desencriptar.py
@@ -4,7 +4,7 @@ from cryptography.fernet import Fernet
 files = []
 
 for file in os.listdir():
-	if file == "voldemort.py" or file == "thekey.key" or file == "decrypt.py": 
+	if file == "iasc.py" or file == "thekey.key" or file == "decrypt.py": 
 		continue
 	if os.path.isfile(file): 
 		files.append(file)
